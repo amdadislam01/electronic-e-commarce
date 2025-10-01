5fffbc48ca2656b5b831ba53186a5c9d4bac988d
amdadislam01 <amdadislam733@gmail.com>
2025-10-01 15:17:15 +0600

    Product features section complate

--- frontend/src/components/ProductFeatures.jsx
+++ frontend/src/components/ProductFeatures.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { TbArrowBackUp } from 'react-icons/tb'
+import { RiSecurePaymentLine } from 'react-icons/ri'
+import { TbArrowBackUp, TbTruckDelivery } from 'react-icons/tb'
 
 const ProductFeatures = () => {
   return (
     <div className='bg-primary rounded-xl mt-6'>
-      <div className="gird grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="">
-            <div className=""><TbArrowBackUp /></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 rounded-xl">
+        <div className="flexCenter gap-x-4 p-2 rounded-3xl">
+            <div className="text-3xl"><TbArrowBackUp className='mb-3 text-yellow-500' /></div>
             <div className="">
-                <h4>Easy Return</h4>
+                <h4 className='h4 capitalize'>Easy Return</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eaque corrupti asperiores animi, praesentium temporibus!</p>
+            </div>
+        </div>
+        <div className="flexCenter gap-x-4 p-2 rounded-3xl">
+            <div className="text-3xl"><TbTruckDelivery className='mb-3 text-red-500' /></div>
+            <div className="">
+                <h4 className='h4 capitalize'>Fast Delivery</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eaque corrupti asperiores animi, praesentium temporibus!</p>
+            </div>
+        </div>
+        <div className="flexCenter gap-x-4 p-2 rounded-3xl">
+            <div className="text-3xl"><RiSecurePaymentLine className='mb-3 text-blue-500' /></div>
+            <div className="">
+                <h4 className='h4 capitalize'>Secure Payment</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eaque corrupti asperiores animi, praesentium temporibus!</p>
             </div>
         </div>
